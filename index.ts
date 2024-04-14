915fe6fc9c63e45b1001942c440e03b5dbdd8911
Abdllauhramdan <abdram2003@gmail.com>
2024-04-14 11:39:39 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+class Contact {
+  name: string;
+  email: string;
+  phone: string;
+
+  constructor(name: string, email: string, phone: string) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+  }
+}
